e505b3586d9f17c8dbe44e68b3f0adbcd1555735
Wahid Narenda Saputra <119840462+WahidNS27@users.noreply.github.com>
2026-04-24 17:03:49 +0700

    Update API base URL to public endpoint

--- frontend-react/src/api/index.js
+++ frontend-react/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://backend-portofolio.nexama.my.id/api',
+  baseURL: 'https://backend-portofolio.nexama.my.id/public/api',
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json',
